f19e6245b2b108895258fb6f6315ff671cc639c9
= <=>
2024-02-28 22:20:00 +0300

    beautiful code that passes the Betty checks

---
+++ betty.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - checks the code
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d ", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
